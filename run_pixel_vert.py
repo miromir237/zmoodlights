e2fa3718986212f2baf18081a2a8410a4cf8f928
miromir237 <miroslav.pilat@gmail.com>
2021-02-17 11:10:04 +0100

    Initial for vertical pixel run.

---
+++ run_pixel_vert.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Running the pixel top down like a faling snow
+
+import colorsys
+import math
+import time
+from random import randint
+
+import unicornhat as unicorn
+
+def clean():
+    unicorn.clear()
+
+unicorn.set_layout(unicorn.AUTO)
+unicorn.rotation(0) # tested on pHAT/HAT with rotation 0, 90, 180 & 270
+unicorn.brightness(0.5)
+u_width,u_height=unicorn.get_shape()
+
+try:
+    while True:
+        x = randint(0, (u_width-1))
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        for y in range(0,u_height,1):
+            unicorn.set_pixel(x, u_height-y-1, r, g, b)
+            #print("y = " + str(u_height-y-1))
+            unicorn.show()
+            time.sleep(0.8)
+            clean()
+        
+except KeyboardInterrupt:
+    pass
