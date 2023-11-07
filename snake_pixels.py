beb35ecbb33dcd300611f9805b8a7d10dd7c132a
Miroslav Pilat <miroslav.pilat@gmail.com>
2023-11-07 06:58:51 +0100

    Snake pixels v1

--- snake_pixels.py
+++ snake_pixels.py
@@ -24,15 +24,30 @@ class LightPoint:
     def __init__(self):
         self.x = randint(0, width - 1)
         self.y = randint(0, height - 1)
-        self.kx = 1
-        self.ky = 0
+        self.kx = 1 if randint(0, 1) == 0 else -1
+        self.ky = 1 if randint(0, 1) == 0 else -1
         self.colour = []
         for i in range(0, 3):
             self.colour.append(randint(100, 255))
 
+    def move(self):
+        self.x += self.kx
+        self.y += self.ky
+        if self.x < 0 or self.x >= width:
+            self.kx *= -1
+        if self.y < 0 or self.y >= height:
+            self.ky *= -1
+
+points = [LightPoint() for _ in range(4)]
 
 try:
     while True:
-        
+        unicorn.clear()
+        for point in points:
+            point.move()
+            unicorn.set_pixel(point.x, point.y, point.colour[0], point.colour[1], point.colour[2])
+        unicorn.show()
+        time.sleep(0.01)
 except KeyboardInterrupt:
-    pass
+    unicorn.clear()
+    unicorn.show()
