085ff6d55ceb8791bb004838c4db697b7fd78198
Miroslav Pilat <miroslav.pilat@gmail.com>
2023-11-07 11:42:25 +0100

    VErtical diraction swap

--- snakev3.py
+++ snakev3.py
@@ -45,12 +45,10 @@ class SnakePixels:
             # If the head of the snake reaches the bottom of the screen, change the direction
             # of the snake and move it one pixel up.
             if self.head[1] >= height:
-                self.head[1] = height - 1
                 self.direction[1] *= -1
             # If the head of the snake reaches the top of the screen, change the direction
             # of the snake and move it one pixel down.
             elif self.head[1] <= 0:
-                self.head[1] = 0
                 self.direction[1] *= -1
         elif self.head[0] >= width:
             self.head[0] = width - 1
@@ -59,12 +57,10 @@ class SnakePixels:
             # If the head of the snake reaches the bottom of the screen, change the direction
             # of the snake and move it one pixel up.
             if self.head[1] >= height:
-                self.head[1] = height - 1
                 self.direction[1] *= -1
             # If the head of the snake reaches the top of the screen, change the direction
             # of the snake and move it one pixel down.
             elif self.head[1] <= 0:
-                self.head[1] = 0
                 self.direction[1] *= -1
         
         # Move body pixels
