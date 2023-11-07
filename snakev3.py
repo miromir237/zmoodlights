121ed0852c54371d4ad63c2d54b7b452fdc53092
Miroslav Pilat <miroslav.pilat@gmail.com>
2023-11-07 12:10:53 +0100

    Fix the condition for height

--- snakev3.py
+++ snakev3.py
@@ -43,13 +43,11 @@ class SnakePixels:
             self.direction[0] = 1
             # If the head of the snake reaches the bottom of the screen, change the direction
             # of the snake and move it one pixel up.
-            if self.head[1] >= height:
-                self.direction[1] = -1
+            if self.head[1] <= height:
                 self.head[1] -= 1
             # If the head of the snake reaches the top of the screen, change the direction
             # of the snake and move it one pixel down.
-            elif self.head[1] <= 0:
-                self.direction[1] = 1
+            elif self.head[1] >= 0:
                 self.head[1] += 1
             
         elif self.head[0] >= width:
@@ -57,13 +55,11 @@ class SnakePixels:
             self.direction[0] = -1
             # If the head of the snake reaches the bottom of the screen, change the direction
             # of the snake and move it one pixel up.
-            if self.head[1] >= height:
-                self.direction[1] = -1
+            if self.head[1] <= height:
                 self.head[1] -= 1
             # If the head of the snake reaches the top of the screen, change the direction
             # of the snake and move it one pixel down.
-            elif self.head[1] <= 0:
-                self.direction[1] = 1
+            elif self.head[1] >= 0:
                 self.head[1] += 1
             
         
