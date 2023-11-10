a782a36c481272c1790b4786a5789f6c07c52441
Miroslav Pilat <miroslav.pilat@gmail.com>
2023-11-10 07:49:39 +0100

    Snake Pixels Service

--- snakes_service.py
+++ snakes_service.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python
 
+# Snakes service
+# Author:  Miroslav Pilat
+# Description: This service will display a snake on the Unicorn HAT.
+#              There are couple movements that the snake can do:
+#                   - The snake will move around the screen and go through the edges.
+#                   - The snake will move around the screen and bounce off the edges.
+#                   - The snake will move veriticaly and bounce off the edges.
+#                   - The snake will move horizontaly and bounce off the edges.
+#              The snake will be made of pixels of two different colors.
+#              
+
+import signal
 import time
 from random import randint
 
 import unicornhat as unicorn
 
+# Class that will be used to gracefully exit the service
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-print("""Snake pixels
-You should see snake moving around the screen and bouncing off the edges.
-If you're using a Unicorn HAT and only half the screen lights up,
-edit this example and  change 'unicorn.AUTO' to 'unicorn.HAT' below.
-""")
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
 
-unicorn.set_layout(unicorn.AUTO)
-unicorn.rotation(0)
-unicorn.brightness(0.3)
-width,height=unicorn.get_shape()
-
-class SnakePixels:
+# Class that will be used to represent the snake and its move around the screen and go through the edges
+class SnakePixelsThrough_edges:
     def __init__(self, x, y):
         self.head = [x, y]
         self.body = [[x - i, y] for i in range(1, 3)]
@@ -25,7 +36,7 @@ class SnakePixels:
         self.head_colour = [0, 0, 255]  # blue color
         self.body_colour = [0, 255, 0]  # blue color
 
-    def move_through_edges(self):
+    def move(self):
         # Save current head position as next position for first body pixel
         next_pos = list(self.head)
         # Update head position
@@ -49,9 +60,17 @@ class SnakePixels:
         self.body.insert(0, next_pos)
         self.body.pop()
 
+# Class that will be used to represent the snake and its move around the screen and go bounce of the edges
+class SnakePixelsBounce_edges:
+    def __init__(self, x, y):
+        self.head = [x, y]
+        self.body = [[x - i, y] for i in range(1, 3)]
+        self.direction = [randint(-1, 1), randint(-1, 1)]  # random direction
+        self.head_colour = [0, 0, 255]  # blue color
+        self.body_colour = [0, 255, 0]  # blue color    
     # This method is used to move the snake around the screen and bounce it
     # off the edges.
-    def move_bounce_edges(self):
+    def move(self):
         # Save current head position as next position for first body pixel
         next_pos = list(self.head)
         # Update head position
@@ -68,6 +87,7 @@ class SnakePixels:
         self.body.insert(0, next_pos)
         self.body.pop()
 
+# Class that will be used to represent the snake and its horizontal movement
 class SnakePixelsHorizontal:
     def __init__(self, x, y):
         self.head = [x, y]
@@ -78,7 +98,7 @@ class SnakePixelsHorizontal:
 
     # This method is used to move the snake around the screen on horizontal lines and bounce it
     # off the edges. When it bounces it also moves one pixel down or up depending on the direction.
-    def move_bounce_horizontal(self):
+    def move(self):
         # Save current head position as next position for first body pixel
         next_pos = list(self.head)
         # Update head position
@@ -109,6 +129,7 @@ class SnakePixelsHorizontal:
         self.body.insert(0, next_pos)
         self.body.pop()
 
+# Class that will be used to represent the snake and its vertical movement
 class SnakePixelsVertical:
     def __init__(self, x, y):
         self.head = [x, y]
@@ -119,7 +140,7 @@ class SnakePixelsVertical:
 
     # This method is used to move the snake around the screen on horizontal lines and bounce it
     # off the edges. When it bounces it also moves one pixel down or up depending on the direction.
-    def move_bounce_vertical(self):
+    def move(self):
         # Save current head position as next position for first body pixel
         next_pos = list(self.head)
         # Update head position
@@ -151,33 +172,61 @@ class SnakePixelsVertical:
         self.body.insert(0, next_pos)
         self.body.pop()        
 
-snake = SnakePixels(randint(0, width - 1), randint(0, height - 1))
+# This function is used to update brightnes according to daytime
+def setBrightness(currenttime):
+  currenthour = currenttime.tm_hour
+  # if it's between 10 am and 8 pm,
+  # use dimmer brightness
+  if(currenthour < 7 or currenthour > 21):
+    unicorn.brightness(0.2)
+  elif (currenthour > 7 and currenthour < 16):
+    unicorn.brightness(0.5)
+  else:
+    unicorn.brightness(0.5)
+
+unicorn.set_layout(unicorn.AUTO)
+unicorn.rotation(0)
+unicorn.brightness(0.3)
+width,height=unicorn.get_shape()
+
+# Create a snake objects
+snake_through = SnakePixelsThrough_edges(randint(0, width - 1), randint(0, height - 1))
+snake_bounce = SnakePixelsBounce_edges(randint(0, width - 1), randint(0, height - 1))
 snake_horizontal = SnakePixelsHorizontal(0, 0)
-snake_vertical = SnakePixelsVertical(7, 3)   
+snake_vertical = SnakePixelsVertical(7, 3)  
 
-try:
-    while True:
-        unicorn.clear()
-        # If direction is [0, 0], generate a new random direction
-#        if snake.direction == [0, 0]:
-#            snake.direction = [randint(-1, 1), randint(-1, 1)]
-#        snake.move_through_edges()
-#        unicorn.set_pixel(snake.head[0], snake.head[1], *snake.head_colour)
-#        for pixel in snake.body:
-#            unicorn.set_pixel(pixel[0], pixel[1], *snake.body_colour)
+# Put snake objects into a list
+snakes = [snake_through, snake_bounce, snake_horizontal, snake_vertical]
 
-        snake_horizontal.move_bounce_horizontal()
-        unicorn.set_pixel(snake_horizontal.head[0], snake_horizontal.head[1], *snake_horizontal.head_colour)
-        for pixel in snake_horizontal.body:
-            unicorn.set_pixel(pixel[0], pixel[1], *snake_horizontal.body_colour)
-
-        snake_vertical.move_bounce_vertical()
-        unicorn.set_pixel(snake_vertical.head[0], snake_vertical.head[1], *snake_vertical.head_colour)
-        for pixel in snake_vertical.body:
-            unicorn.set_pixel(pixel[0], pixel[1], *snake_vertical.body_colour)
-
-        unicorn.show()
-        time.sleep(0.1)
-except KeyboardInterrupt:
+def main():
+    setBrightness(time.localtime())
     unicorn.clear()
-    unicorn.show()
+
+    # Set timer to 90 seconds
+    timer = 90
+    
+    # Randomly pick a snake from the list of snakes
+    active_snake = snakes[randint(0, len(snakes) - 1)]
+        
+    # While timer is not 0, move the snake around the screen
+    while timer > 0:
+        # Move the snake around the screen
+        active_snake.move()
+        # Plot the snake on the Unicorn HAT
+        unicorn.set_pixel(active_snake.head[0], active_snake.head[1], *active_snake.head_colour)
+        for pixel in active_snake.body:
+            unicorn.set_pixel(pixel[0], pixel[1], *active_snake.body_colour)
+        # Show the snake on the Unicorn HAT
+        unicorn.show()
+        # Sleep for 0.1 seconds
+        time.sleep(0.1)
+        # Decrease timer by 1
+        timer -= 1
+
+
+if __name__ == '__main__':
+  killer = GracefulKiller()
+  while not killer.kill_now:
+      main()
+
+  print("End of the program. I was killed gracefully :)")
