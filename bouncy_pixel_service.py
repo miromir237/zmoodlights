ebf8f0795310f92cec90a6d78142f5751ab5237e
Miroslav Pilat <miroslav.pilat@gmail.com>
2023-12-11 07:50:59 +0100

    Color randomness.

--- bouncy_pixel_service.py
+++ bouncy_pixel_service.py
@@ -20,6 +20,7 @@ width,height=unicorn.get_shape()
 
 points = []
 
+# GracefulKiller class from https://stackoverflow.com/a/31464349
 class GracefulKiller:
   kill_now = False
   def __init__(self):
@@ -29,6 +30,7 @@ class GracefulKiller:
   def exit_gracefully(self,signum, frame):
     self.kill_now = True
 
+# LightPoint class
 class LightPoint:
     def __init__(self):
         self.x = randint(0, width - 1)
@@ -39,6 +41,9 @@ class LightPoint:
         for i in range(0, 3):
             self.colour.append(randint(100, 255))
 
+# Functions
+
+# update_positions() moves the points around
 def update_positions():
     for point in points:
         if ((point.y + point.ky) > height - 1) or ((point.y + point.ky) < 0):
@@ -50,6 +55,7 @@ def update_positions():
         point.x += point.kx
         point.y += point.ky
 
+# update_colours() changes the colours of the points
 def update_colours():
     for point in points:
         for i in range(0, 3):
@@ -58,33 +64,44 @@ def update_colours():
                 point.colour[i] = 255
             elif point.colour[i] < 0:
                 point.colour[i] = 0
-                
+
+# plot_points() plots the points on the Unicorn HAT                
 def plot_points():
     unicorn.clear()
     for point in points:
         unicorn.set_pixel(point.x, point.y, point.colour[0], point.colour[1], point.colour[2])
     unicorn.show()
 
+# setBrightness() sets the brightness of the Unicorn HAT
 def setBrightness(currenttime):
   currenthour = currenttime.tm_hour
-  # if it's between 10 am and 8 pm,
+  # if it's between 9 pm and 7 am,
   # use dimmer brightness
   if(currenthour < 7 or currenthour > 21):
     unicorn.brightness(0.2)
-  elif (currenthour > 7 and currenthour < 16):
-    unicorn.brightness(0.5)
   else:
     unicorn.brightness(0.5)
 
+# main() is the main function
 def main():
+    # set brightness
     setBrightness(time.localtime())
+
+    # add a new point every once in a while
     if len(points) < 5 and randint(0, 5) > 1:
         points.append(LightPoint())
+
+    # plot_points() every time
     plot_points()
+
+    # update_positions() every time
     update_positions()
+
     # update_colours() once in a while
     if randint(0, 10) > 8:
         update_colours()
+
+    # sleep for 0.3 seconds befor next iteration    
     time.sleep(0.3)
 
 if __name__ == '__main__':
